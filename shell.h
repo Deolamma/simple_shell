f0f7c12e1d58e496104aadbbfee6e4d2e1ab9248
= <=>
2023-07-18 18:05:31 +0100

    header file containing prototypes

--- shell.h
+++ shell.h
@@ -1,8 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include <stddef.h>
+#define BUFFSIZ 1024
+/* MAIN FXN -  functions */
+void interactive_mode(void);
+void non_interactive_mode(void);
 
-int _strcmp(const char*, const char*);
-char *_strcpy(char*, const char*);
-char *_strcat(char*, const char*);
+/* string helpers */
+int _strcmp(const char *, const char *);
+char *_strcpy(char *, const char *);
+char *_strcat(char *, const char *);
+size_t _strlen(const char *);
+char *str_tok(char *, const char *);
+ssize_t _getline(char **, size_t *, FILE *);
 
 #endif
