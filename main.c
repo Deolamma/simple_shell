a87b036288357355ba2e71890404f780bb6cbb33
= <=>
2023-07-18 18:04:13 +0100

    entry point into the simple_shell

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
+#include "shell.h"
+#include <unistd.h>
 /**
  * main - Entry point into the simple_shell project
+ * it checks if we are in interactive or non-interactive mode
  * Return: 0 on Success
  */
 int main(void)
 {
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		interactive_mode();
+	}
+	non_interactive_mode();
 	return (0);
 }
 
