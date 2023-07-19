bca81fcdcf4274984c68bbd5d3aad767a4b83327
= <=>
2023-07-19 18:37:41 +0100

    splits commands entered into tokens

---
+++ _tokenize.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _tokenize - Breaks strings recieved as it's param into tokens
+ * @line: string to be tokenized
+ * Return: returns an array of strings(tokens or cmds) to be executed
+ */
+char **_tokenize(char *line)
+{
+	char **tokens;
+	char *token;
+	int i = 0, tokens_buff = TOKENS_BUFF;
+
+
+	tokens = malloc(sizeof(char *) * tokens_buff);
+	if (!tokens)
+	{
+		free(line);
+		dprintf(STDERR_FILENO, "Memory allocation failed: _tokenize");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, DELIM);
+	while (token)
+	{
+		tokens[i] = token;
+		i++;
+		if (i >= tokens_buff)
+		{
+			tokens_buff *= 2;
+			tokens = realloc(tokens, sizeof(char *) * tokens_buff);
+			if (!tokens)
+			{
+				free(line);
+				dprintf(STDERR_FILENO, "Memory realloc failed: _tokenize");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, DELIM);
+	}
+	tokens[i] = '\0';
+	return (tokens);
+}
