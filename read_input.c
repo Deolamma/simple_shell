e1a74aa81889aa3edc148a8f183709d940f3487d
= <=>
2023-07-19 18:40:14 +0100

    receives commands from user before tokenization

---
+++ read_input.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_input - reads the command or text written to the terminal
+ * Return: returns the string inputted by user
+ */
+char *read_input()
+{
+	char *line = NULL;
+	size_t buffsize = 0;
+
+	if (getline(&line, &buffsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			free(line);
+			exit(EXIT_SUCCESS);
+		} else
+		{
+			free(line);
+			perror("Unable to read input");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
+}
+
