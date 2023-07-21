a906422387b9e28806ed8e1fffde139682968ca2
= <=>
2023-07-21 16:18:18 +0100

    Executes command if user entered the abs path

---
+++ abs_path_cmd_runner.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * abs_path_cmd_runner - execute the commands entered by user
+ * @cmds: commands entered by user broken down into tokens
+ * Return: always (1), enables loop
+ */
+int abs_path_cmd_runner(char **cmds)
+{
+	pid_t pid;
+	int status;
+
+	if (cmds[0] == NULL)
+	{
+		/* User pressed enter key */
+		return (1);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		free(cmds);
+		perror("Forking failed");
+	}
+	if (pid == 0)
+	{
+		if (execve(cmds[0], cmds, NULL) == -1)
+			perror("Invalid commands");
+		free(cmds);
+		exit(EXIT_FAILURE);
+	}
+	do {
+		waitpid(pid, &status, WUNTRACED);
+	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+	return (1);
+}
