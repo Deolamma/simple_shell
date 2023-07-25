96dd4b783f1c81ce53ceb06dbf74a44d6cfb02cc
= <=>
2023-07-25 10:09:23 +0100

    Implements the cd built-in

---
+++ cd_built_in.c
@@ -0,0 +1,108 @@
+#include "shell.h"
+/**
+ * cd_home - changes dir to home dir, if no arg is passed to cd
+ * @args_cpy: command entered by user
+ * Return: return an int value
+ */
+int cd_home(char **args_cpy)
+{
+	char *home_path;
+	char *home_path_cpy;
+
+	home_path = _getenv("HOME");
+	if (!home_path)
+	{
+		perror(args_cpy[1]);
+		free(args_cpy);
+		return (1);
+	}
+	home_path_cpy = _strdup(home_path);
+	if (!home_path_cpy)
+	{
+		perror(args_cpy[1]);
+		free(args_cpy);
+		return (1);
+	}
+	if (chdir(home_path_cpy) != 0)
+	{
+		perror(args_cpy[1]);
+		free(home_path_cpy);
+		free(args_cpy);
+		return (1);
+	}
+	free(home_path_cpy);
+	free(args_cpy);
+	return (1);
+}
+/**
+ * cd_old_wd - return to old working dir if args[1] == "-"
+ * @args_cpy: command entered by user
+ * Return: return 1
+ */
+int cd_old_wd(char **args_cpy)
+{
+	char *old_wd;
+	char *old_wd_cpy;
+
+	old_wd = _getenv("OLDPWD");
+	if (!old_wd)
+	{
+		perror(args_cpy[1]);
+		free(args_cpy);
+		return (1);
+	}
+	old_wd_cpy = _strdup(old_wd);
+	if (!old_wd_cpy)
+	{
+		perror(args_cpy[1]);
+		free(args_cpy);
+		return (1);
+	}
+	if (chdir(old_wd_cpy) != 0)
+	{
+		perror(args_cpy[1]);
+		free(old_wd_cpy);
+		free(args_cpy);
+		return (1);
+	}
+	free(old_wd_cpy);
+	free(args_cpy);
+	return (1);
+}
+/**
+ * _cd - my custom change directory
+ * @args: commands entered by the user
+ * Return: returns 1, a signal to loop again
+ */
+int _cd(char **args)
+{
+	char **args_copy = NULL;
+
+	if (args[1] == NULL)
+	{
+		args_copy = _strdup_arr(args);
+		if (!args_copy)
+		{
+			perror("memory allocation failed: args_copy");
+			return (1);
+		}
+		cd_home(args_copy);
+	} else if (_strcmp(args[1], "-") != 0)
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror(args[1]);
+			return (1);
+		}
+	} else
+	{
+		args_copy = _strdup_arr(args);
+		if (!args_copy)
+		{
+			perror("memory allocation failed: args_copy");
+			return (1);
+		}
+		cd_old_wd(args_copy);
+	}
+	return (1);
+}
