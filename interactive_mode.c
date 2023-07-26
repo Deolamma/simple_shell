6d32b6bc1109606247e59aa12b4f8b822e3625e9
= <=>
2023-07-26 19:14:47 +0100

    Updated to handle signal from Ctrl+C

--- interactive_mode.c
+++ interactive_mode.c
@@ -1,22 +1,31 @@
 #include "shell.h"
-#include <stdlib.h>
-#include <unistd.h>
 /**
  * interactive_mode - This mode user enters command from inside our code
  */
 void interactive_mode(void)
 {
 	char **cmds;
-	char *line;
+	char *line = NULL;
 	int status;
+	struct sigaction sig_act;
 
+	sig_act.sa_flags = SA_SIGINFO;
+	sig_act.sa_sigaction = sigint_handler;
+	sigemptyset(&sig_act.sa_mask);
+	if (sigaction(SIGINT, &sig_act, (void *)&line) == -1)
+	{
+		perror("Sigaction failed");
+		free(line);
+		exit(EXIT_FAILURE);
+	}
 	do {
-		write(STDOUT_FILENO, "$ ", 3);
+		write(STDOUT_FILENO, "$ ", 2);
 		line = read_input();
 		cmds = _tokenize(line);
 		status = process_handler(cmds);
 	free(line);
+	line = NULL;
 	free(cmds);
-	} while (status);
+	} while (status == 1);
 }
 
